affb4061571cce3c6b1c8dca4feb086f8a081953
Asran2017 <ranju86100@gmail.com>
2025-07-24 06:12:01 +0530

    Third Commit

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ const donationContent = document.querySelector(".section--6");
 const samajamContent = document.querySelector(".section--7");
 const contactContent = document.querySelector(".section--8");
 window.onload = function () {
-  introContent.style.display = "block";
-  mantraContent.style.display = "block";
+  introContent.style.display = "none";
+  mantraContent.style.display = "none";
   introContent.classList.add("section-animate");
   mantraContent.classList.add("section-animate");
   historyContent.style.display = "none";
@@ -24,82 +24,133 @@ window.onload = function () {
   samajamContent.style.display = "none";
   contactContent.style.display = "none";
 };
-historyLink.addEventListener("click", function (e) {
-  e.preventDefault();
-  itenaryContent.style.display = "none";
-  historyContent.style.display = "block";
-  historyContent.classList.add("section-animate");
-  scrollText.textContent =
-    " ஸர்வத்ர கோவிந்த நாம சங்கீர்த்தனம்....கோவிந்தா கோவிந்தா...!!";
-  introContent.style.display = "none";
-  mantraContent.style.display = "none";
-  donationContent.style.display = "none";
-  samajamContent.style.display = "none";
-  contactContent.style.display = "none";
-});
-homeLink.addEventListener("click", function (e) {
-  e.preventDefault();
-  historyContent.style.display = "none";
-  itenaryContent.style.display = "none";
-  donationContent.style.display = "none";
-  samajamContent.style.display = "none";
-  contactContent.style.display = "none";
-  scrollText.textContent =
-    "ஹரே ராம ஹரே ராம ராம ஹரே ஹரே... ஹரே கிருஷ்ண ஹரே கிருஷ்ண கிருஷ்ண ஹரே ஹரே!!";
-  introContent.style.display = "block";
-  mantraContent.style.display = "block";
-  introContent.classList.add("section-animate");
-  mantraContent.classList.add("section-animate");
+// historyLink.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   itenaryContent.style.display = "none";
+//   historyContent.style.display = "block";
+//   historyContent.classList.add("section-animate");
+//   scrollText.textContent =
+//     " ஸர்வத்ர கோவிந்த நாம சங்கீர்த்தனம்....கோவிந்தா கோவிந்தா...!!";
+//   introContent.style.display = "none";
+//   mantraContent.style.display = "none";
+//   donationContent.style.display = "none";
+//   samajamContent.style.display = "none";
+//   contactContent.style.display = "none";
+// });
+// homeLink.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   historyContent.style.display = "none";
+//   itenaryContent.style.display = "none";
+//   donationContent.style.display = "none";
+//   samajamContent.style.display = "none";
+//   contactContent.style.display = "none";
+//   scrollText.textContent =
+//     "ஹரே ராம ஹரே ராம ராம ஹரே ஹரே... ஹரே கிருஷ்ண ஹரே கிருஷ்ண கிருஷ்ண ஹரே ஹரே!!";
+//   introContent.style.display = "block";
+//   mantraContent.style.display = "block";
+//   introContent.classList.add("section-animate");
+//   mantraContent.classList.add("section-animate");
+// });
+
+// itenaryLink.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   itenaryContent.style.display = "flex";
+//   itenaryContent.classList.add("section-animate");
+//   scrollText.textContent =
+//     "ஓம் தாமோதராய வித்மஹே  ருக்மணி வல்லபாய தீமஹி  தந்நோ கிருஷ்ண: ப்ரசோதயாத்...";
+//   introContent.style.display = "none";
+//   mantraContent.style.display = "none";
+//   historyContent.style.display = "none";
+//   donationContent.style.display = "none";
+//   samajamContent.style.display = "none";
+//   contactContent.style.display = "none";
+// });
+// samajamLink.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   samajamContent.style.display = "block";
+//   samajamContent.classList.add("section-animate");
+//   scrollText.textContent =
+//     "ஸ்ரீ க்ருஷ்ண கோவிந்த ஹரே முராரே....ஹே நாத நாராயண வாசுதேவ..!!";
+//   introContent.style.display = "none";
+//   mantraContent.style.display = "none";
+//   donationContent.style.display = "none";
+//   historyContent.style.display = "none";
+//   itenaryContent.style.display = "none";
+//   contactContent.style.display = "none";
+// });
+
+// donationLink.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   donationContent.style.display = "block";
+//   donationContent.classList.add("section-animate");
+//   scrollText.textContent =
+//     "அச்யுதம் கேஷவம் ராம நாராயணம்...கிருஷ்ண தாமோதரம் வாசுதேவம் ஹரி..!!";
+//   introContent.style.display = "none";
+//   mantraContent.style.display = "none";
+//   historyContent.style.display = "none";
+//   itenaryContent.style.display = "none";
+//   samajamContent.style.display = "none";
+//   contactContent.style.display = "none";
+// });
+// contactLink.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   contactContent.style.display = "block";
+//   contactContent.classList.add("section-animate");
+//   introContent.style.display = "none";
+//   mantraContent.style.display = "none";
+//   historyContent.style.display = "none";
+//   itenaryContent.style.display = "none";
+//   samajamContent.style.display = "none";
+//   donationContent.style.display = "none";
+// });
+const sectionArray = [
+  "introContent",
+  "mantraContent",
+  "historyContent",
+  "itenaryContent",
+  "donationContent",
+  "samajamContent",
+  "contactContent",
+];
+const idArray = [
+  "home_link",
+  "history_link",
+  "itenary_link",
+  "donation_link",
+  "samajam_link",
+  "contact_link",
+];
+let clickedLink;
+idArray.forEach((elt) => {
+  clickedLink = document.getElementById(`${elt}`);
+  console.log(clickedLink);
+  clickedLink.addEventListener("click", function (e) {
+    // alert("clicked");
+    clickedId = e.currentTarget.id;
+    console.log(clickedId);
+    if (clickedId === idArray[0]) {
+      `${sectionArray[0]}`.style.display = "block";
+      sectionArray[1].style.display = "block";
+    } else if (clickedId === idArray[1]) {
+      // historyContent.style.display = "block";
+    }
+  });
 });
 
-itenaryLink.addEventListener("click", function (e) {
-  e.preventDefault();
-  itenaryContent.style.display = "flex";
-  itenaryContent.classList.add("section-animate");
-  scrollText.textContent =
-    "ஓம் தாமோதராய வித்மஹே  ருக்மணி வல்லபாய தீமஹி  தந்நோ கிருஷ்ண: ப்ரசோதயாத்...";
-  introContent.style.display = "none";
-  mantraContent.style.display = "none";
-  historyContent.style.display = "none";
-  donationContent.style.display = "none";
-  samajamContent.style.display = "none";
-  contactContent.style.display = "none";
-});
-samajamLink.addEventListener("click", function (e) {
-  e.preventDefault();
-  samajamContent.style.display = "block";
-  samajamContent.classList.add("section-animate");
-  scrollText.textContent =
-    "ஸ்ரீ க்ருஷ்ண கோவிந்த ஹரே முராரே....ஹே நாத நாராயண வாசுதேவ..!!";
-  introContent.style.display = "none";
-  mantraContent.style.display = "none";
-  donationContent.style.display = "none";
-  historyContent.style.display = "none";
-  itenaryContent.style.display = "none";
-  contactContent.style.display = "none";
-});
+//      clickedId = e.currentTarget.id;
+//     if (clickedId === idArray[0]) {
+//       sectionArray[0].style.display = "block";
+//       sectionArray[1].style.display = "block";
+//     } else if (clickedId === idArray[1]) {
+//       sectionArray[3].style.display = "block";
+//     }
+//   });
+// });
 
-donationLink.addEventListener("click", function (e) {
-  e.preventDefault();
-  donationContent.style.display = "block";
-  donationContent.classList.add("section-animate");
-  scrollText.textContent =
-    "அச்யுதம் கேஷவம் ராம நாராயணம்...கிருஷ்ண தாமோதரம் வாசுதேவம் ஹரி..!!";
-  introContent.style.display = "none";
-  mantraContent.style.display = "none";
-  historyContent.style.display = "none";
-  itenaryContent.style.display = "none";
-  samajamContent.style.display = "none";
-  contactContent.style.display = "none";
-});
-contactLink.addEventListener("click", function (e) {
-  e.preventDefault();
-  contactContent.style.display = "block";
-  contactContent.classList.add("section-animate");
-  introContent.style.display = "none";
-  mantraContent.style.display = "none";
-  historyContent.style.display = "none";
-  itenaryContent.style.display = "none";
-  samajamContent.style.display = "none";
-  donationContent.style.display = "none";
-});
+// historyLink.addEventListener("click", function (event) {
+//   event.preventDefault;
+
+//   let id = event.target.id;
+//   document.getElementById(`${id}`);
+//   console.log(document.getElementById(`${id}`));
+// });
