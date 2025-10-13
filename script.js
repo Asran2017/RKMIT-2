c140301123c7d4973288a05650fe6ea4eafd74c0
Asran2017 <ranju86100@gmail.com>
2025-10-13 11:20:14 +0530

    Initial commit

--- script.js
+++ script.js
@@ -17,129 +17,129 @@ const samajamContent = document.querySelector(".section--7");
 const contactContent = document.querySelector(".section--8");
 const galleryContent = document.querySelector(".section--9");
 
-const sectionDisplay = () => {
-  e.preventDefault();
-  const clickedLink = e.target.id;
-  sectionArray.forEach((elt) => {
-    if (clickedLink === idArray[1] && elt === sectionArray[2]) {
-      historyContent.style.display = "block";
-      homePage.style.display = "none";
-    }
-  });
-};
-// window.onload = function () {
-//   introContent.style.display = "block";
-
-//   // introContent.classList.add("section_visible");
-//   // console.log(introContent);
-//   mantraContent.style.display = "block";
-//   introContent.classList.add("section-animate");
-//   mantraContent.classList.add("section-animate");
-//   historyContent.style.display = "none";
-//   itenaryContent.style.display = "none";
-//   donationContent.style.display = "none";
-//   samajamContent.style.display = "none";
-//   contactContent.style.display = "none";
-//   galleryContent.style.display = "none";
+// const sectionDisplay = () => {
+//   e.preventDefault();
+//   const clickedLink = e.target.id;
+//   sectionArray.forEach((elt) => {
+//     if (clickedLink === idArray[1] && elt === sectionArray[2]) {
+//       historyContent.style.display = "block";
+//       homePage.style.display = "none";
+//     }
+//   });
 // };
-// historyLink.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   itenaryContent.style.display = "none";
-//   historyContent.style.display = "block";
-//   historyContent.classList.add("section-animate");
-//   scrollText.textContent =
-//     " ஸர்வத்ர கோவிந்த நாம சங்கீர்த்தனம்....கோவிந்தா கோவிந்தா...!!";
-//   introContent.style.display = "none";
-//   mantraContent.style.display = "none";
-//   donationContent.style.display = "none";
-//   samajamContent.style.display = "none";
-//   contactContent.style.display = "none";
-//   galleryContent.style.display = "none";
-// });
-// homeLink.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   historyContent.style.display = "none";
-//   itenaryContent.style.display = "none";
-//   donationContent.style.display = "none";
-//   samajamContent.style.display = "none";
-//   contactContent.style.display = "none";
-//   galleryContent.style.display = "none";
-//   scrollText.textContent =
-//     "ஹரே ராம ஹரே ராம ராம ஹரே ஹரே... ஹரே கிருஷ்ண ஹரே கிருஷ்ண கிருஷ்ண ஹரே ஹரே!!";
-//   introContent.style.display = "block";
-//   mantraContent.style.display = "block";
-//   introContent.classList.add("section-animate");
-//   mantraContent.classList.add("section-animate");
-// });
+window.onload = function () {
+  introContent.style.display = "block";
 
-// itenaryLink.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   itenaryContent.style.display = "flex";
-//   itenaryContent.classList.add("section-animate");
-//   scrollText.textContent =
-//     "ஓம் தாமோதராய வித்மஹே  ருக்மணி வல்லபாய தீமஹி  தந்நோ கிருஷ்ண: ப்ரசோதயாத்...";
-//   introContent.style.display = "none";
-//   mantraContent.style.display = "none";
-//   historyContent.style.display = "none";
-//   donationContent.style.display = "none";
-//   samajamContent.style.display = "none";
-//   contactContent.style.display = "none";
-//   galleryContent.style.display = "none";
-// });
-// samajamLink.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   samajamContent.style.display = "block";
-//   samajamContent.classList.add("section-animate");
-//   scrollText.textContent =
-//     "ஸ்ரீ க்ருஷ்ண கோவிந்த ஹரே முராரே....ஹே நாத நாராயண வாசுதேவ..!!";
-//   introContent.style.display = "none";
-//   mantraContent.style.display = "none";
-//   donationContent.style.display = "none";
-//   historyContent.style.display = "none";
-//   itenaryContent.style.display = "none";
-//   contactContent.style.display = "none";
-//   galleryContent.style.display = "none";
-// });
+  // introContent.classList.add("section_visible");
+  // console.log(introContent);
+  mantraContent.style.display = "block";
+  introContent.classList.add("section-animate");
+  mantraContent.classList.add("section-animate");
+  historyContent.style.display = "none";
+  itenaryContent.style.display = "none";
+  donationContent.style.display = "none";
+  samajamContent.style.display = "none";
+  contactContent.style.display = "none";
+  galleryContent.style.display = "none";
+};
+historyLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  itenaryContent.style.display = "none";
+  historyContent.style.display = "block";
+  historyContent.classList.add("section-animate");
+  scrollText.textContent =
+    " ஸர்வத்ர கோவிந்த நாம சங்கீர்த்தனம்....கோவிந்தா கோவிந்தா...!!";
+  introContent.style.display = "none";
+  mantraContent.style.display = "none";
+  donationContent.style.display = "none";
+  samajamContent.style.display = "none";
+  contactContent.style.display = "none";
+  galleryContent.style.display = "none";
+});
+homeLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  historyContent.style.display = "none";
+  itenaryContent.style.display = "none";
+  donationContent.style.display = "none";
+  samajamContent.style.display = "none";
+  contactContent.style.display = "none";
+  galleryContent.style.display = "none";
+  scrollText.textContent =
+    "ஹரே ராம ஹரே ராம ராம ஹரே ஹரே... ஹரே கிருஷ்ண ஹரே கிருஷ்ண கிருஷ்ண ஹரே ஹரே!!";
+  introContent.style.display = "block";
+  mantraContent.style.display = "block";
+  introContent.classList.add("section-animate");
+  mantraContent.classList.add("section-animate");
+});
 
-// donationLink.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   donationContent.style.display = "block";
-//   donationContent.classList.add("section-animate");
-//   scrollText.textContent =
-//     "அச்யுதம் கேஷவம் ராம நாராயணம்...கிருஷ்ண தாமோதரம் வாசுதேவம் ஹரி..!!";
-//   introContent.style.display = "none";
-//   mantraContent.style.display = "none";
-//   historyContent.style.display = "none";
-//   itenaryContent.style.display = "none";
-//   samajamContent.style.display = "none";
-//   contactContent.style.display = "none";
-//   galleryContent.style.display = "none";
-// });
-// contactLink.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   contactContent.style.display = "block";
-//   contactContent.classList.add("section-animate");
-//   introContent.style.display = "none";
-//   mantraContent.style.display = "none";
-//   historyContent.style.display = "none";
-//   itenaryContent.style.display = "none";
-//   samajamContent.style.display = "none";
-//   donationContent.style.display = "none";
-//   galleryContent.style.display = "none";
-// });
+itenaryLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  itenaryContent.style.display = "flex";
+  itenaryContent.classList.add("section-animate");
+  scrollText.textContent =
+    "ஓம் தாமோதராய வித்மஹே  ருக்மணி வல்லபாய தீமஹி  தந்நோ கிருஷ்ண: ப்ரசோதயாத்...";
+  introContent.style.display = "none";
+  mantraContent.style.display = "none";
+  historyContent.style.display = "none";
+  donationContent.style.display = "none";
+  samajamContent.style.display = "none";
+  contactContent.style.display = "none";
+  galleryContent.style.display = "none";
+});
+samajamLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  samajamContent.style.display = "block";
+  samajamContent.classList.add("section-animate");
+  scrollText.textContent =
+    "ஸ்ரீ க்ருஷ்ண கோவிந்த ஹரே முராரே....ஹே நாத நாராயண வாசுதேவ..!!";
+  introContent.style.display = "none";
+  mantraContent.style.display = "none";
+  donationContent.style.display = "none";
+  historyContent.style.display = "none";
+  itenaryContent.style.display = "none";
+  contactContent.style.display = "none";
+  galleryContent.style.display = "none";
+});
 
-// galleryLink.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   galleryContent.style.display = "grid";
-//   galleryContent.classList.add("section-animate");
-//   introContent.style.display = "none";
-//   mantraContent.style.display = "none";
-//   historyContent.style.display = "none";
-//   itenaryContent.style.display = "none";
-//   samajamContent.style.display = "none";
-//   donationContent.style.display = "none";
-//   contactContent.style.display = "none";
-// });
+donationLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  donationContent.style.display = "block";
+  donationContent.classList.add("section-animate");
+  scrollText.textContent =
+    "அச்யுதம் கேஷவம் ராம நாராயணம்...கிருஷ்ண தாமோதரம் வாசுதேவம் ஹரி..!!";
+  introContent.style.display = "none";
+  mantraContent.style.display = "none";
+  historyContent.style.display = "none";
+  itenaryContent.style.display = "none";
+  samajamContent.style.display = "none";
+  contactContent.style.display = "none";
+  galleryContent.style.display = "none";
+});
+contactLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  contactContent.style.display = "block";
+  contactContent.classList.add("section-animate");
+  introContent.style.display = "none";
+  mantraContent.style.display = "none";
+  historyContent.style.display = "none";
+  itenaryContent.style.display = "none";
+  samajamContent.style.display = "none";
+  donationContent.style.display = "none";
+  galleryContent.style.display = "none";
+});
+
+galleryLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  galleryContent.style.display = "grid";
+  galleryContent.classList.add("section-animate");
+  introContent.style.display = "none";
+  mantraContent.style.display = "none";
+  historyContent.style.display = "none";
+  itenaryContent.style.display = "none";
+  samajamContent.style.display = "none";
+  donationContent.style.display = "none";
+  contactContent.style.display = "none";
+});
 const sectionArray = [
   "introContent",
   "mantraContent",
@@ -190,4 +190,3 @@ const idArray = [
 //   let id = event.target.id;
 //   document.getElementById(`${id}`);
 //   console.log(document.getElementById(`${id}`));
-// });
