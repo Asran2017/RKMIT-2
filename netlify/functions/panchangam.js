5225f06b105df7966b9eaff37dcfe84dcc37c5d7
Ranjani <ranju86100@gmail.com>
2026-04-02 11:56:59 +0530

    UPdated

--- netlify/functions/panchangam.js
+++ netlify/functions/panchangam.js
@@ -31,12 +31,21 @@ export async function handler() {
 
     const data = await apiResponse.json();
     console.log(data);
+    const panchang = data.data;
+
+    const result = {
+      day: panchang.vaara,
+      tithi: panchang.tithi[0].name,
+      nakshatra: panchang.nakshatra[0].name,
+      date: new Date(panchang.sunrise).toLocaleDateString("en-IN"),
+    };
 
     // STEP 3: Send data to frontend
     return {
       statusCode: 200,
       body: JSON.stringify(data),
     };
+    console.log(data);
   } catch (error) {
     return {
       statusCode: 500,
